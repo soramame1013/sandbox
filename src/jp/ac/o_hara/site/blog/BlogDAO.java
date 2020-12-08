2db2c5dba896549d18dfd299eccf907d58acfdd8
student <student@kas-it2-pc11.o-hara-kashiwa.local>
2020-12-08 15:04:08 +0900

    ブログの部分を仮実装

---
+++ src/jp/ac/o_hara/site/blog/BlogDAO.java
@@ -0,0 +1,34 @@
+package jp.ac.o_hara.site.blog;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import jp.ac.o_hara.site.SimpleDAO;
+public class BlogDAO extends SimpleDAO {
+	private static BlogDAO dao = new BlogDAO();
+	private BlogDAO() {
+	}
+	public static BlogDAO getInstanse() {
+		return dao;
+	}
+	public boolean create(BlogBean blog) {
+		Connection db = this.createConnection();
+		//PreparedStatement ps = null;
+		boolean result = false;
+		try (PreparedStatement ps = db
+				.prepareStatement("INSERT INTO blogtbl(userName, date, title, article) VALUES(?, ?, ?, ?)")) {
+			//ps = db.prepareStatement("INSERT INTO user(realName, userID, passwd) VALUES(?, ?, ?)");
+			ps.setString(1, blog.getUserId());
+			ps.setDate(2, blog.getDate());
+			ps.setString(3, blog.getTitle());
+			ps.setString(3, blog.getArticle());
+			ps.executeUpdate();
+			result = true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			this.closeConnection(db);
+		}
+		return result;
+	}
+}
---
+++ test/jp/ac/o_hara/site/blog/BlogTestListener.java
@@ -0,0 +1,32 @@
+package jp.ac.o_hara.site.blog;
+import java.sql.Date;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+@WebListener
+public class BlogTestListener implements ServletContextListener {
+	BlogDAO dao = null;
+	public BlogTestListener() {
+		dao = BlogDAO.getInstanse();
+	}
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		if (System.getenv("DATABASE_URL") != null) {
+		} else {
+			if (dao.execSQL("CREATE TABLE IF NOT EXISTS blogtbl"
+					+ " (id IDENTITY , userID VARCHAR(64), date DATE , title VARCHAR(64), article VARCHAR(256))")) {
+				System.out.println("TestBlogDB is READY.");
+			} else {
+				System.out.println("TestUserDB is NOT READY.");
+			}
+		}
+		dao.create(new BlogBean("hoge", Date.valueOf("2020-12-08"), "ほげ", "ほげほげ"));
+		dao.create(new BlogBean("piyo", Date.valueOf("2020-12-07"), "ぴよ", "ぴよぴよ"));
+		//4つテスト用に作る
+	}
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		dao = null;
+	}
+}
